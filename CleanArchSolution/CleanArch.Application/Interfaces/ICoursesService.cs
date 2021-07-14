357224d92be1a4f9602043492d3d563568a6eca4
kebbeboayue <Kebbe.Boayue@ibm.com>
2021-07-14 09:31:49 -0500

    Commit implementation of service and repository.

--- CleanArchSolution/CleanArch.Application/Interfaces/ICoursesService.cs
+++ CleanArchSolution/CleanArch.Application/Interfaces/ICoursesService.cs
@@ -9,6 +9,6 @@ namespace CleanArch.Application.Interfaces
 {
     public interface ICoursesService
     {
-        IEnumerable<CourseViewModel> GetCourses();
+        CourseViewModel GetCourses();
     }
 }
---
+++ CleanArchSolution/CleanArch.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Data.Context;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private CleanArchDbContext _contxt;
+        public CourseRepository(CleanArchDbContext cleanArchDbContext)
+        {
+            _contxt = cleanArchDbContext;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _contxt.Courses;
+        }
+    }
+}
